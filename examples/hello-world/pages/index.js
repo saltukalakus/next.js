fc603bcf9f0a771043af9e1661e0302e449a7034
Saltuk Alakus <saltukalakus@gmail.com>
2020-06-26 23:52:15 +0300

    hello

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -1,12 +1,21 @@
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export default function IndexPage() {
-  return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
-  )
+function RedirectPage({ ctx }) {
+  const router = useRouter()
 }
+
+RedirectPage.getInitialProps = ctx => {
+  // We check for ctx.res to make sure we're on the server.
+  if (ctx.res) {
+    ctx.res.writeHead(302, {
+      'Vary': 'Accept',
+      'Access-Control-Allow-Origin': 'https://next-js-hello.vercel.app',
+      'Content-Type': 'text/html; charset=utf-8',
+      Location: 'auth0.samples.Auth0Sample://ticket-00456618.us.auth0.com/ios/auth0.samples.Auth0Sample/callback'
+    });
+    ctx.res.end(body);
+  }
+  return {};
+}
+
+export default RedirectPage
