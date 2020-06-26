485826b6901ad0a05aafea00c28813341402b465
Saltuk Alakus <saltukalakus@gmail.com>
2020-06-26 23:56:27 +0300

    ıuıuı

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -9,11 +9,10 @@ RedirectPage.getInitialProps = ctx => {
   if (ctx.res) {
     ctx.res.writeHead(302, {
       'Vary': 'Accept',
-      'Access-Control-Allow-Origin': 'https://next-js-hello.vercel.app',
       'Content-Type': 'text/html; charset=utf-8',
       Location: 'auth0.samples.Auth0Sample://ticket-00456618.us.auth0.com/ios/auth0.samples.Auth0Sample/callback'
     });
-    ctx.res.end(body);
+    ctx.res.end();
   }
   return {};
 }
